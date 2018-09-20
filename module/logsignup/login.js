171f32478b045618e8630c18f63dad12c15012b4
dianligegege <1220671124@qq.com>
2018-09-20 14:51:14 +0800

    up2

--- module/logsignup/login.js
+++ module/logsignup/login.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 //管理员登录  各种路由处理
 router.get('/', (req, res)=>{
-    // res.render('login');
-    res.send('管理员登录')
+    res.render('logsignup/login')
 });
 
 
--- module/logsignup/signup.js
+++ module/logsignup/signup.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 //管理员登录  各种路由处理
 router.get('/', (req, res)=>{
-    // res.render('login');
-    res.send('注册页面')
+    res.render('logsignup/signin')
 });
-
 
 
 
